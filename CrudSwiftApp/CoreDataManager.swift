7fba5aafe95580923fecabd68c305e19bc223777
Armando Salcido <17171342@itculiacan.edu.mx>
2022-12-05 02:18:52 -0800

    Terminacion coredata y correcion de errores

--- CrudSwiftApp/CoreDataManager.swift
+++ CrudSwiftApp/CoreDataManager.swift
@@ -19,7 +19,7 @@ class CoreDataManager {
                 fatalError("Core data failed to inicialited \(error.localizedDescription)") }
         })
     }
-    func guardarProducto(clv_obra: Int32,clv_viga: Int32, longitud:Double, material:String,peso:Double){
+    func guardarViga(clv_obra: Int32,clv_viga: Int32, longitud:Double, material:String,peso:Double){
         let viga = Viga(context: persistentContainer.viewContext)
         viga.clv_obra = clv_obra
         viga.clv_viga = clv_viga
@@ -35,5 +35,41 @@ class CoreDataManager {
             print("Failed to save error en \(error)")
         }
     }
-    
+    func leerViga() -> [Viga]{
+        let fetchRequest : NSFetchRequest <Viga> = Viga.fetchRequest()
+        do{
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        }
+        catch{
+            return[]
+        }
+    }
+    func eliminarViga(Viga:Viga){
+        persistentContainer.viewContext.delete(Viga)
+        do{
+            try persistentContainer.viewContext.save()
+        }
+        catch{
+            persistentContainer.viewContext.rollback()
+            print("Ha ocurrido un error \(error.localizedDescription)")
+        }
+    }
+    func actualizarViga(clv_obra: Int32,clv_viga: Int32, longitud:Double, material:String,peso:Double){
+        let fetchRequest : NSFetchRequest<Viga> = Viga.fetchRequest()
+        let predicate = NSPredicate(format: "clViga = %@", clv_viga ?? "")
+        fetchRequest.predicate = predicate
+        
+        do{
+            let datos = try persistentContainer.viewContext.fetch(fetchRequest)
+            let v = datos.first
+            v?.clv_obra = clv_obra
+            v?.clv_viga = clv_viga
+            v?.longitud = longitud
+            v?.material = material
+            v?.peso = peso
+        }
+        catch{
+            print("Error en \(error)")
+        }
+    }
 }
