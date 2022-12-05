96391dfe16985553fca9d7588dfc21e9e72e8f67
Armando Salcido <17171342@itculiacan.edu.mx>
2022-12-04 20:37:18 -0800

    Initial Commit

---
+++ CrudSwiftApp/CrudSwiftAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CrudSwiftAppApp.swift
+//  CrudSwiftApp
+//
+//  Created by Armando Salcido on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CrudSwiftAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
