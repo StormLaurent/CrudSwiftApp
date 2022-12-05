5472b7600dfbdf8b19a3cf28e3cfb3a796704534
Armando Salcido <17171342@itculiacan.edu.mx>
2022-12-04 23:14:54 -0800

    prueba en la v1

--- CrudSwiftApp/CrudSwiftAppApp.swift
+++ CrudSwiftApp/CrudSwiftAppApp.swift
@@ -15,7 +15,7 @@ struct CrudSwiftAppApp: App {
         WindowGroup {
            // ContentView()
                // .environment(\.managedObjectContext, persistenceController.container.viewContext)
-            
+            // ESTO ES SOLO UNA PRUEBA
         }
     }
 }
