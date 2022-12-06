5448db5223c4125ae5051dc469135d068ca917a0
Armando Salcido <17171342@itculiacan.edu.mx>
2022-12-06 01:21:53 -0800

    implemetacion de boton y limpieza de los textfield

--- CrudSwiftApp/ContentView.swift
+++ CrudSwiftApp/ContentView.swift
@@ -22,7 +22,37 @@ struct ContentView: View {
     
     var body: some View{
         VStack{
-            TextField("clv_obra", text: $clv_obra).textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Clave Obra", text: $clv_obra).textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Clave Viga", text: $clv_viga).textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Longitud", text:$longitud).textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Material", text:$material).textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Peso", text:$peso).textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save"){
+                if(seleccionado != nil){
+                    seleccionado?.clv_obra = clv_obra
+                    seleccionado?.clv_viga = clv_viga
+                    seleccionado?.longitud = longitud
+                    seleccionado?.material = material
+                    seleccionado?.peso = peso
+                    coreDM.actualizarViga(clv_viga: seleccionado!)
+                }
+                else {
+                    coreDM.guardarViga(clv_obra:clv_obra,clv_viga: clv_viga,longitud: longitud,material:material,peso: peso)
+                    
+                }
+                mostrarViga()
+                clv_obra = ""
+                clv_viga = ""
+                longitud = ""
+                material = ""
+                peso = ""
+                
+            }
         }
+        
     }
+    func mostrarViga(){
+        vigasArray = coreDM.leerViga()
+    }
+    
 }
